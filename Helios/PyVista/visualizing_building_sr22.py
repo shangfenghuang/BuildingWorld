139ed06abda11b9ae13d53f360e36bb5d7b9054e
feng <45654745+shangfenghuang@users.noreply.github.com>
2025-05-24 16:49:03 -0600

    PyVista

---
+++ Helios/PyVista/visualizing_building_sr22.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+# ---------------------------------------------------
+# @Time    : 2025-05-24 12:37 a.m.
+# @Author  : shangfeng
+# @Organization: University of Calgary
+# @File    : visualizing_building_sr22.py
+# @IDE     : PyCharm
+# ---------------------------------------------------
+import pyvista as pv
+
+# Load the OBJ file
+mesh = pv.read(r"C:\Users\12617\Desktop\Montreal_Helios_shapefile_test\Montreal_Helios_test.obj")
+
+# Create a plotter and add the mesh
+plotter = pv.Plotter(window_size=(1000, 800))
+# plotter.set_background("white")  # 可选背景色
+plotter.add_axes()
+
+# add mesh model
+plotter.add_mesh(mesh, color="lightgray")
+
+plotter.show()
--- Helios/PyVista/visualizing_sr22.py
+++ Helios/PyVista/visualizing_sr22.py
@@ -4,7 +4,7 @@
 # @Time    : 2025-05-23 5:04 p.m.
 # @Author  : shangfeng
 # @Organization: University of Calgary
-# @File    : visualizing_obj.py
+# @File    : visualizing_sr22.py
 # @IDE     : PyCharm
 # ---------------------------------------------------
 import trimesh
